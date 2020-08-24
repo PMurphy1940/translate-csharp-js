ae7366ad5e49e1c3fb79e17735bca6a79a1fede8
Patrick Murphy <engine_ear@mac.com>
2020-08-24 15:53:38 -0500

    FizzBuzz complete

---
+++ fizzbuzz/csharp/fizzbuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int count = 1; count < 101; count++)
+            {
+                if (count % 15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (count % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (count % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine($"{count}");
+                }
+            }
+        }
+    }
+}
